3b603b34e412c71916009086af5c5a8a8d9b3070
Eugene Kos <mstrsid@live.com>
2020-08-06 20:33:14 +0300

    Base done

--- app/src/main/java/com/example/colordescriptor/MainActivity.java
+++ app/src/main/java/com/example/colordescriptor/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.colordescriptor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinnerColors;
+    private TextView textViewDescriptionTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinnerColors = findViewById(R.id.spinnerColors);
+        textViewDescriptionTemp = findViewById(R.id.textViewDescriptionTemp);
+    }
+
+    public void showDescription(View view) {
+        int position = spinnerColors.getSelectedItemPosition();
+        textViewDescriptionTemp.setText(getDescription(position));
+    }
+
+    public String getDescription(int position){
+        String[] descriptions = getResources().getStringArray(R.array.descriptions);
+        return descriptions[position];
     }
 }
